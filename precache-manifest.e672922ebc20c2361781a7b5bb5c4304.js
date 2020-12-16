c25104edb5d88e0a5a9cf327cd2b813bb98a39ea
Abbas <alokhandwala@deqode.com>
2020-12-17 00:04:58 +0530

    Updates

---
+++ precache-manifest.e672922ebc20c2361781a7b5bb5c4304.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fdd42efa17a10482250cfbacba2ebd3",
+    "url": "/abbas-task/index.html"
+  },
+  {
+    "revision": "d3f1d294962469b15ee7",
+    "url": "/abbas-task/static/css/main.31c221da.chunk.css"
+  },
+  {
+    "revision": "c9aff3110e14e9b6420c",
+    "url": "/abbas-task/static/js/2.564141cf.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/abbas-task/static/js/2.564141cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3f1d294962469b15ee7",
+    "url": "/abbas-task/static/js/main.83ecfae2.chunk.js"
+  },
+  {
+    "revision": "81947194f49db557b88d",
+    "url": "/abbas-task/static/js/runtime-main.19e55094.js"
+  }
+]);
